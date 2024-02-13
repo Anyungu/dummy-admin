52bda0614bf7fd2d966e8c5ba5431ae98ee6ce11
Anyungu Wanyungu <anyungucw@gmail.com>
2024-02-13 10:23:59 +0300

    next navigation

--- app/login/components/SignIn.tsx
+++ app/login/components/SignIn.tsx
@@ -15,7 +15,7 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 
 
