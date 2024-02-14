660e0909faeabc4ef3df48ccffa4c15013415a70
Anyungu Wanyungu <anyungucw@gmail.com>
2024-02-14 10:02:42 +0300

    Side nav deploy

--- app/(dashboard)/sidenav.tsx
+++ app/(dashboard)/sidenav.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { usePathname } from 'next/navigation';
-import { CalendarIcon, HomeIcon, IdCardIcon, PersonIcon, TriangleDownIcon, TriangleUpIcon } from '@radix-ui/react-icons';
+import { CalendarIcon, CopyIcon, HomeIcon, IdCardIcon, PersonIcon, TokensIcon, TriangleDownIcon, TriangleUpIcon } from '@radix-ui/react-icons';
 
 
 const IS_ACTIVE = `flex transition-colors duration-300 ease-in-out text-black items-center px-5 py-3 space-x-2 hover:bg-primaryGreen bg-primaryGreen cursor-pointer w-full`
@@ -13,36 +13,48 @@ function Sidenav() {
   const isActive = (path: string) => pathName == path;
 
   return (
-    <div className="w-64 bg-primaryDark text-white flex flex-col font-semibold py-16">
+    <div className="tabs:w-64 bg-primaryDark text-white flex flex-col font-semibold py-16">
       <div onClick={(e) => { e.preventDefault(); setPageDown(prevState => !prevState) }} className={isActive('/pages/homepage') ? IS_ACTIVE : IS_NOT_ACTIVE}>
         <div className='w-full flex flex-row items-center justify-between'>
           <div className='flex flex-row items-center space-x-2'>
-            <HomeIcon />
-            <div>Pages</div>
+            <CopyIcon />
+            <div className='hidden tabs:block'>Pages</div>
           </div>
           <div>
             {pageDown ? <TriangleUpIcon /> : <TriangleDownIcon />}
           </div>
         </div>
       </div>
-      {pageDown && <div className={`pl-10 ${isActive('/pages/homepage') ? IS_ACTIVE : IS_NOT_ACTIVE}`}>
-        <Link href="/pages/homepage">Homepage</Link>
-      </div>}
-      {pageDown && <div className={`pl-10 ${isActive('/pages/categories') ? IS_ACTIVE : IS_NOT_ACTIVE}`}>
-        <Link href="/pages/categories">Categories</Link>
-      </div>}
-      <div className={isActive('/events') ? IS_ACTIVE : IS_NOT_ACTIVE}>
-        <CalendarIcon />
-        <Link href="/events">Events</Link>
-      </div>
-      <div className={isActive('/tickets') ? IS_ACTIVE : IS_NOT_ACTIVE}>
-        <IdCardIcon />
-        <Link href="/tickets">Tickets</Link>
-      </div>
-      <div className={isActive('/users') ? IS_ACTIVE : IS_NOT_ACTIVE}>
-        <PersonIcon />
-        <Link href="/users">Users</Link>
-      </div>
+      {pageDown && <Link href="/pages/homepage" className={`pl-10 ${isActive('/pages/homepage') ? IS_ACTIVE : IS_NOT_ACTIVE}`}>
+        <div className='flex flex-row items-center space-x-2'>
+          <HomeIcon />
+          <div className='hidden tabs:block'> Homepage </div>
+        </div>
+      </Link>}
+      {pageDown && <Link href="/pages/categories" className={`pl-10 ${isActive('/pages/categories') ? IS_ACTIVE : IS_NOT_ACTIVE}`}>
+        <div className='flex flex-row items-center space-x-2'>
+          <TokensIcon />
+          <div className='hidden tabs:block'> Categories </div>
+        </div>
+      </Link>}
+      <Link href="/events" className={isActive('/events') ? IS_ACTIVE : IS_NOT_ACTIVE}>
+        <div className='flex flex-row items-center space-x-2'>
+          <CalendarIcon />
+          <div className='hidden tabs:block'> Events</div>
+        </div>
+      </Link>
+      <Link href="/tickets" className={isActive('/tickets') ? IS_ACTIVE : IS_NOT_ACTIVE}>
+        <div className='flex flex-row items-center space-x-2'>
+          <IdCardIcon />
+          <div className='hidden tabs:block'>Tickets</div>
+        </div>
+      </Link>
+      <Link href="/users" className={isActive('/users') ? IS_ACTIVE : IS_NOT_ACTIVE}>
+        <div className='flex flex-row items-center space-x-2'>
+          <PersonIcon />
+          <div className='hidden tabs:block'>Users</div>
+        </div>
+      </Link>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,12 @@ const config = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+    screens: {
+      mobile: "620px",
+      tabs: "768px",
+      desktop: "1200px",
+      largeDesktop: "1400px",
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
